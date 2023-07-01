b9b58af24c79ed02c8f88f053e1e60599ccd8048
Bao-thik-coding <113531784+Bao-thik-coding@users.noreply.github.com>
2023-07-01 21:07:39 +0700

    Add project files.

---
+++ Nhập_tên/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhập_tên
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Nhập Họ và tên:  ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Tên Bạn là  " + Name);
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
